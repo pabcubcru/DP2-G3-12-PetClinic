d0dee22b3af93ec44d677413f54005ece4d87c84
Pablo <pabgarcer@alum.us.es>
2020-03-10 16:45:15 +0100

    Added functionalities of Hospitalisation

---
+++ src/main/java/org/springframework/samples/petclinic/web/HospitalisationController.java
@@ -0,0 +1,71 @@
+package org.springframework.samples.petclinic.web;
+
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Hospitalisation;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.service.HospitalisationService;
+import org.springframework.samples.petclinic.service.PetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pets/{petId}")
+public class HospitalisationController {
+	
+	private HospitalisationService hospitalisationService;
+	private PetService petService;
+	
+	@Autowired
+	public HospitalisationController(final HospitalisationService hospitalisationService, final PetService petService) {
+		this.hospitalisationService = hospitalisationService;
+		this.petService = petService;
+	}
+	
+	@InitBinder
+	public void setAllowedFields(WebDataBinder dataBinder) {
+		dataBinder.setDisallowedFields("id");
+	}
+	
+	@ModelAttribute("hospitalisation")
+	public Hospitalisation loadPetWithHospitalisation(@PathVariable("petId") int petId) {
+		Pet pet = this.petService.findPetById(petId);
+		Hospitalisation hospitalisation = new Hospitalisation();
+		pet.addHospitalisation(hospitalisation);
+		return hospitalisation;
+	}
+	
+	@GetMapping(value = "/owners/*/pets/{petId}/hospitalisations/new")
+	public String initNewHospitalisationForm(@PathVariable("petId") int petId, Map<String, Object> model) {
+		return "pets/createOrUpdateHospitalisationForm";
+	}
+	
+	@PostMapping(value = "/owners/{ownerId}/pets/{petId}/hospitalisations/new")
+	public String processNewVisitForm(@Valid Hospitalisation hospitalisation, BindingResult result) {
+		if (result.hasErrors()) {
+			return "pets/createOrUpdateHospitalisationForm";
+		}
+		else {
+			this.petService.saveHospitalisation(hospitalisation);
+			return "redirect:/owners/{ownerId}";
+		}
+	}
+	
+	@GetMapping(value = "/owners/*/pets/{petId}/hospitalisations")
+	public String showHospitalisations(@PathVariable int petId, Map<String, Object> model) {
+		model.put("hospitalisations", this.petService.findPetById(petId).getHospitalisations());
+		return "hospitalisationList";
+	}
+	
+}
