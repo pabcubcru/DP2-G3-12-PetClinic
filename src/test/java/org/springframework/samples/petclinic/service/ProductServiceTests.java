0b35d6ea5067eaae47f710f5afa5e26ead4a7483
Pablo <pabcubcru@alum.us.es>
2020-03-23 18:59:47 +0100

    Finalizada Task020ServiceTest

--- src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ProductServiceTests.java
@@ -16,6 +16,7 @@
 package org.springframework.samples.petclinic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
@@ -29,11 +30,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Discount;
 import org.springframework.samples.petclinic.model.Product;
 import org.springframework.samples.petclinic.model.Shop;
-import org.springframework.samples.petclinic.service.exceptions.DuplicatedProductNameException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -80,113 +79,95 @@ class ProductServiceTests {
 	@Autowired
 	protected DiscountService discountService;
 
+//	FIND PRODUCT BY ID
+
 	@Test
-	void shouldFindProductWithCorrectId() {
-
+	void shouldFindProductWithCorrectId() throws Exception {
 		Product product2 = this.productService.findProductById(2);
-
 		assertThat(product2.getStock()).isEqualTo(10);
 		assertThat(product2.getPrice()).isEqualTo(25.0);
 	}
 
+//	FIND PRODUCTS NAMES
+
 	@Test
-	void shoudFindProductsNames() {
-
+	void shoudFindProductsNames() throws Exception {
 		List<String> products = this.productService.findProductsNames();
-
 		assertThat(products.contains("product1")).isTrue();
 		assertThat(products.contains("product4")).isFalse();
 	}
 
-	@Test
-	void shouldAddNewProduct() {
+//	 INSERT PRODUCT
 
+	@Test
+	void shouldInsertNewProduct() throws Exception {
 		List<String> products = this.productService.findProductsNames();
 		int tamaño = products.size();
-
 		Shop shop1 = shopService.findShops().iterator().next();
 		Product product = new Product();
 		product.setName("product3");
 		product.setPrice(50.0);
 		product.setStock(20);
 		shop1.addProduct(product);
-
 		try {
 			this.productService.saveProduct(product);
-		} catch (DuplicatedProductNameException ex) {
+		} catch (Exception ex) {
 			Logger.getLogger(ProductServiceTests.class.getName()).log(Level.SEVERE, null, ex);
 		}
-
 		products = this.productService.findProductsNames();
-
 		assertThat(products.size()).isEqualTo(tamaño + 1);
 		assertThat(product.getId()).isNotNull();
-	}	
-	
-	@Test
-	void shouldThrowExceptionAddingNewProduct() {
+	}
 
+	@Test
+	void shouldThrowExceptionInsertingNewProductNullParameter() throws Exception {
 		Shop shop1 = shopService.findShops().iterator().next();
 		Product product = new Product();
 		product.setName("product3");
 		product.setPrice(null);
 		product.setStock(20);
 		shop1.addProduct(product);
+		assertThrows(ConstraintViolationException.class, () -> {
+			this.productService.saveProduct(product);
+		});
+	}
 
-		assertThrows(Exception.class, () -> {
-		this.productService.saveProduct(product);
-	});
-	}	
-	
 	@Test
-	void shouldThrowDuplicatedProductNameExceptionAddingNewProduct() {
-
+	void shouldThrowExceptionInsertingNewProductDuplicatedName() throws Exception {
 		Shop shop1 = shopService.findShops().iterator().next();
 		Product product = new Product();
 		product.setName("product2");
 		product.setPrice(50.0);
 		product.setStock(20);
 		shop1.addProduct(product);
-
-//		assertThrows(DuplicatedProductNameException.class, () -> {
-//		this.productService.saveProduct(product);
-//	});
-		
-		try {
+		assertThrows(Exception.class, () -> {
 			this.productService.saveProduct(product);
-		} catch (DuplicatedProductNameException ex) {
-			Logger.getLogger(ProductServiceTests.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}	
-	
-	
-	
-	
+		});
+	}
+
+//	 DELETE PRODUCT
 
 	@Test
 	@Transactional
-	public void shouldDeleteProduct() {
-
+	public void shouldDeleteProduct() throws Exception {
 		List<String> products = this.productService.findProductsNames();
 		Product product1 = this.productService.findProductById(1);
 		int tamaño = products.size();
-
 		Shop shop1 = shopService.findShops().iterator().next();
 		shop1.deleteProduct(product1);
-
 		if (product1.getDiscount() != null) {
 			discountService.deleteDiscount(product1.getDiscount().getId());
 		}
 		productService.deleteProduct(product1);
 		products = this.productService.findProductsNames();
-
 		assertThat(products.size()).isEqualTo(tamaño - 1);
-
 	}
+
+//	 EDIT PRODUCT
 
 	@Test
 	@Transactional
-	public void shouldUpdateProductName() throws Exception {
+	public void shouldEditProductName() throws Exception {
 		Product product1 = this.productService.findProductById(1);
 		String oldName = product1.getName();
 
@@ -195,7 +176,7 @@ class ProductServiceTests {
 
 		try {
 			this.productService.saveProduct(product1);
-		} catch (DuplicatedProductNameException ex) {
+		} catch (Exception ex) {
 			Logger.getLogger(ProductServiceTests.class.getName()).log(Level.SEVERE, null, ex);
 		}
 		product1 = this.productService.findProductById(1);
@@ -204,32 +185,82 @@ class ProductServiceTests {
 
 	@Test
 	@Transactional
-	public void shouldThrowExceptionUpdatingProductNull() throws Exception { // MAAAAAAAAL
+	public void shouldThrowExceptionEditingProductNullParameter() throws Exception {
 		Product product1 = this.productService.findProductById(1);
-
-		product1.setPrice(null);
-
-		assertThrows(ConstraintViolationException.class, () -> {
+		assertThrows(Exception.class, () -> {
+			product1.setPrice(null);
 			this.productService.saveProduct(product1);
 		});
-		
 	}
-	
+
 	@Test
 	@Transactional
-	public void shouldThrowExceptionUpdatingProductDuplicatedName() throws DuplicatedProductNameException { // MAAAAAL
+	public void shouldThrowExceptionEditingProductDuplicatedName() throws Exception {
 		Product product1 = this.productService.findProductById(1);
-		
-		assertThrows(DuplicatedProductNameException.class, () -> {product1.setName("product2");
+		assertThrows(Exception.class, () -> {
+			product1.setName("product2");
 			this.productService.saveProduct(product1);
 		});
 	}
 
+//	 FIND PRODUCTS BY NAME
+
 	@Test
-	void shouldFindProductByName() {
+	void shouldFindProductByName() throws Exception {
 		Product product1 = this.productService.findByName("product1");
 		assertThat(product1.getStock()).isEqualTo(5);
 		assertThat(product1.getPrice()).isEqualTo(15.0);
 	}
 
+//	INSERT DISCOUNT
+
+	@Test
+	void shouldInsertDiscountForProduct() throws Exception {
+		Product product2 = this.productService.findByName("product2");
+		Discount discount = new Discount();
+		discount.setFinishDate(LocalDate.now().plusDays(2));
+		discount.setStartDate(LocalDate.now());
+		discount.setName("test discount");
+		discount.setPercentage(50.0);
+		this.discountService.saveDiscount(discount);
+		product2.setDiscount(discount);
+		product2 = this.productService.findByName("product2");
+		assertThat(discount.getId()).isNotNull();
+		assertThat(product2.getDiscount()).isNotNull();
+	}
+
+	@Test
+	void shouldThowsExceptionInsertingDiscountNullParameter() throws Exception {
+		Discount discount = new Discount();
+		discount.setFinishDate(LocalDate.now().plusDays(2));
+		discount.setStartDate(LocalDate.now());
+		discount.setName("test discount");
+		assertThrows(Exception.class, () -> {
+			discount.setPercentage(null);
+			this.discountService.saveDiscount(discount);
+		});
+	}
+
+//	EDIT DISCOUNT
+
+	@Test
+	void shouldEditDiscountForProduct() throws Exception {
+		Product product1 = this.productService.findByName("product1");
+		Discount discount = this.discountService.findDiscountById(1).get();
+		discount.setFinishDate(LocalDate.now().plusDays(2));
+		this.discountService.saveDiscount(discount);
+		product1.setDiscount(discount);
+		product1 = this.productService.findByName("product1");
+		assertThat(product1.getDiscount()).isNotNull();
+		assertThat(product1.getDiscount().getFinishDate()).isEqualTo(LocalDate.now().plusDays(2));
+	}
+
+	@Test
+	void shouldThrowsExceptionEditingDiscountNullParameter() throws Exception {
+		Discount discount = this.discountService.findDiscountById(1).get();
+		assertThrows(Exception.class, () -> {
+			discount.setPercentage(null);
+			this.discountService.saveDiscount(discount);
+		});
+	}
 }
