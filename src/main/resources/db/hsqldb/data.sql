7ce0a577dca06ced8109e3e04adf893a3efd752a
Pablo <pabcubcru@alum.us.es>
2020-04-22 15:22:40 +0200

    Updated data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -92,6 +92,6 @@ INSERT INTO products(id,name,price,stock,shop_id,discount_id) VALUES (5, 'produc
 INSERT INTO orders(id,name,supplier,product_number,order_date,order_status,shop_id,product_id) VALUES (1, 'order1', 'supplier1', 10, CURRENT_TIMESTAMP, 0, 1, 2);
 INSERT INTO orders(id,name,supplier,product_number,order_date,order_status,shop_id,product_id) VALUES (2, 'order2', 'supplier2', 25, '2020-03-10 13:30', 0, 1, 2);
 INSERT INTO orders(id,name,supplier,product_number,order_date,order_status,shop_id,product_id) VALUES (3, 'order3', 'supplier3', 10, '2020-03-12 12:30', 1, 1, 4);
-INSERT INTO orders(id,name,supplier,product_number,order_date,order_status,shop_id,product_id) VALUES (4, 'order4', 'supplier4', 25, '2020-04-20 13:30', 0, 1, 4);
+INSERT INTO orders(id,name,supplier,product_number,order_date,order_status,shop_id,product_id) VALUES (4, 'order4', 'supplier4', 25, '2020-03-20 13:30', 0, 1, 4);
 INSERT INTO orders(id,name,supplier,product_number,order_date,order_status,shop_id,product_id) VALUES (5, 'order5', 'supplier5', 30, '2020-04-20 13:30', 1, 1, 4);
 
