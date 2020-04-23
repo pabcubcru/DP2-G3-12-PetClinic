56855466efe17e7441e15d9d778872320b1c8ad2
marqu <marqu@DESKTOP-6MRJTJH>
2020-04-23 17:58:21 +0200

    Terminado test integración de DiscountController

--- src/test/java/org/springframework/samples/petclinic/web/integration/DiscountControllerIntegrationTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/integration/DiscountControllerIntegrationTest.java
@@ -1,10 +1,8 @@
 package org.springframework.samples.petclinic.web.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.time.LocalDate;
 import java.util.Collections;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +10,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.model.Discount;
 import org.springframework.samples.petclinic.model.Product;
+import org.springframework.samples.petclinic.model.Shop;
+import org.springframework.samples.petclinic.service.DiscountService;
 import org.springframework.samples.petclinic.service.ProductService;
+import org.springframework.samples.petclinic.service.ShopService;
 import org.springframework.samples.petclinic.web.DiscountController;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.ui.ModelMap;
@@ -29,6 +30,12 @@ public class DiscountControllerIntegrationTest {
 	
 	@Autowired
 	private ProductService productService;
+	
+	@Autowired
+	private ShopService shopService;
+	
+	@Autowired
+	private DiscountService discountService;
 
 	@Test
 	void testInitNewDiscountForm() throws Exception {
@@ -37,19 +44,81 @@ public class DiscountControllerIntegrationTest {
 		
 		assertEquals(view, "discounts/createOrUpdateDiscountForm");
 	}
+
 	
 	@Test
 	void testProcessNewDiscountFormSuccess() throws Exception {
 		Discount discount = new Discount();
-		discount.setFinishDate(LocalDate.of(2020, 4, 23));
+		discount.setFinishDate(LocalDate.of(2020, 10, 23));
 		discount.setStartDate(LocalDate.of(2020, 7, 11));
 		discount.setPercentage(20.0);
 		BindingResult result = new MapBindingResult(Collections.emptyMap(), "");
+		Shop shop = shopService.findShopById(1);
+		Product product = productService.findProductById(2);
+		String view = discountController.processNewDiscountForm(discount, result, product, shop.getId());
+		
+		assertEquals(view, "redirect:/shops/1/products/{productId}");
+		
+	}
+	
+
+	@Test
+	void testProcessNewDiscountFormHasErrors() throws Exception {
+		Discount discount = new Discount();
+		discount.setFinishDate(LocalDate.of(2020, 10, 23));
+		discount.setStartDate(LocalDate.of(2020, 7, 11));
+		discount.setPercentage(null);
+		BindingResult result = new MapBindingResult(Collections.emptyMap(), "");
+		result.rejectValue("percentage", "nullPercentage");
 		Product product = productService.findProductById(2);
 		int shopId = 1;
 		String view = discountController.processNewDiscountForm(discount, result, product, shopId);
 		
-		assertEquals(view, "redirect:/shops/" + shopId + "/products/{productId}");
+		assertEquals(view, "discounts/createOrUpdateDiscountForm");
+		assertEquals(result.getFieldErrorCount("percentage"), 1); 
+	}
+	
+
+	@Test
+	void testInitEditDiscountForm() throws Exception {
+		ModelMap model = new ModelMap();
+		Discount discount = discountService.findDiscountById(1);
+		Product product1 = new Product();
+		model.put("discount", discount);
+		String view = discountController.initEditDiscountForm(model, product1, discount.getId());
 		
+		assertEquals(view, "discounts/createOrUpdateDiscountForm");
+	}
+
+	
+	@Test
+	void testProcessEditDiscountFormSuccess() throws Exception {
+		Discount discount = new Discount();
+		discount.setStartDate(LocalDate.of(2020, 6, 10));
+		discount.setFinishDate(LocalDate.of(2020, 7, 10));
+		discount.setPercentage(30.0);
+		BindingResult result = new MapBindingResult(Collections.emptyMap(), "");
+		Shop shop = shopService.findShopById(1);
+		Product product = productService.findProductById(2);
+		String view = discountController.processNewDiscountForm(discount, result, product, shop.getId());
+		
+		
+		assertEquals(view, "redirect:/shops/1/products/{productId}");
+	}
+	
+	@Test
+	void testProcessEditDiscountFormHasErrors() throws Exception {
+		Discount discount = new Discount();
+		discount.setFinishDate(LocalDate.of(2020, 10, 23));
+		discount.setStartDate(LocalDate.of(2020, 7, 11));
+		discount.setPercentage(null);
+		BindingResult result = new MapBindingResult(Collections.emptyMap(), "");
+		result.rejectValue("percentage", "nullPercentage");
+		Product product = productService.findProductById(2);
+		int shopId = 1;
+		String view = discountController.processNewDiscountForm(discount, result, product, shopId);
+		
+		assertEquals(view, "discounts/createOrUpdateDiscountForm");
+		assertEquals(result.getFieldErrorCount("percentage"), 1);
 	}
 }
