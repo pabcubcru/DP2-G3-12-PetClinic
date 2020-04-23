e1d5f494416b07d9447d77b7ba6f7b7aa51a49c3
alemandor1 <alemandor1@alum.us.es>
2020-04-23 18:50:56 +0200

    Task 059 done, E2E stayController

--- src/test/java/org/springframework/samples/petclinic/web/e2e/StayControllerE2ETest.java
+++ src/test/java/org/springframework/samples/petclinic/web/e2e/StayControllerE2ETest.java
@@ -1,10 +1,7 @@
-package org.springframework.samples.petclinic.web;
+package org.springframework.samples.petclinic.web.e2e;
 
 import static org.hamcrest.Matchers.hasProperty;
-
 import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -14,24 +11,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.Stay;
-import org.springframework.samples.petclinic.service.PetService;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = StayController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
-public class StayControllerTest {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+
+public class StayControllerE2ETest {
 
 	private static final int TEST_PET_ID_1 = 1;
 	private static final int TEST_PET_ID_2 = 2;
@@ -39,66 +32,23 @@ public class StayControllerTest {
 	private static final int TEST_STAY_ID_2 = 2;
 	private static final int TEST_STAY_ID_3 = 3;
 	private static final int TEST_STAY_ID_4 = 4;
+	private static final int TEST_STAY_ID_5 = 5;
+	private static final int TEST_STAY_ID_7 = 7;
 
 	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
-	private PetService clinicService;
-
-	private Stay testStay;
-
-	@BeforeEach
-	void setup() {
-		testStay = new Stay();
-		testStay.setStartdate(LocalDate.of(2020, 07, 15));
-		testStay.setFinishdate(LocalDate.of(2020, 07, 19));
-		testStay.setPet(clinicService.findPetById(TEST_PET_ID_1));
-		testStay.setId(TEST_STAY_ID_1);
-		testStay.setPrice(30.0);
-		testStay.setSpecialCares("test special cares");
-
-		Stay stay2 = new Stay();
-		stay2.setId(TEST_STAY_ID_2);
-		stay2.setStartdate(LocalDate.of(2020, 07, 20));
-		stay2.setFinishdate(LocalDate.of(2020, 07, 25));
-		
-		Stay stay3 = new Stay();
-		stay3.setId(TEST_STAY_ID_3);
-		stay3.setStartdate(LocalDate.of(2020, 01, 01));
-		stay3.setFinishdate(LocalDate.of(2020, 01, 10));
-		
-		Stay stay4 = new Stay();
-		stay4.setId(TEST_STAY_ID_4);
-		stay4.setStartdate(LocalDate.of(2020, 04, 01));
-		stay4.setFinishdate(LocalDate.of(2021, 10, 10));
-
-		Pet pet1 = new Pet();
-		pet1.setId(TEST_PET_ID_1);
-		Pet pet2 = new Pet();
-		pet2.setId(TEST_PET_ID_2);
-		
-		given(this.clinicService.findStayById(TEST_STAY_ID_1)).willReturn(this.testStay);
-		given(this.clinicService.findStayById(TEST_STAY_ID_2)).willReturn(stay2);
-		given(this.clinicService.findPetById(TEST_PET_ID_1)).willReturn(pet1);
-		given(this.clinicService.findPetById(TEST_PET_ID_2)).willReturn(pet2);
-		given(this.clinicService.findStaysByPetId(TEST_PET_ID_1)).willReturn(Lists.emptyList());
-		given(this.clinicService.findStaysByPetId(TEST_PET_ID_2)).willReturn(Lists.list(testStay, stay2));
-		given(this.clinicService.findStayById(TEST_STAY_ID_3)).willReturn(stay3);
-		given(this.clinicService.findStayById(TEST_STAY_ID_4)).willReturn(stay4);
-	}
-
-	
 	// INSERTAR ESTANCIA
+
 	
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testInitNewStayForm() throws Exception {
 		mockMvc.perform(get("/owners/*/pets/{petId}/stays/new", TEST_PET_ID_1)).andExpect(status().isOk())
 				.andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessNewStayFormSuccess() throws Exception {
 		mockMvc.perform(post("/owners/1/pets/{petId}/stays/new", TEST_PET_ID_1).with(csrf())
@@ -107,7 +57,7 @@ public class StayControllerTest {
 				.andExpect(view().name("redirect:/owners/{ownerId}"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessNewStayFormHasErrorsFinishDateBeforeStartDateAndPriceNull() throws Exception {
 		mockMvc.perform(
@@ -118,19 +68,28 @@ public class StayControllerTest {
 				.andExpect(model().attributeHasFieldErrorCode("stay", "finishdate", "dateStartDateAfterDateFinishDate"))
 				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
-	
-	@WithMockUser(value = "spring")
+
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessNewStayFormHasErrorsStartDateInPast() throws Exception {
-		mockMvc.perform(
-				post("/owners/1/pets/{petId}/stays/new", TEST_PET_ID_1).with(csrf()).param("startdate", "2020/03/06").param("price", "15.0")
-						.param("finishdate", "2020/06/04").param("specialCares", "A lot of special cares"))
-				.andExpect(model().attributeHasErrors("stay"))
+		mockMvc.perform(post("/owners/1/pets/{petId}/stays/new", TEST_PET_ID_1).with(csrf())
+				.param("startdate", "2020/03/06").param("price", "15.0").param("finishdate", "2020/06/04")
+				.param("specialCares", "A lot of special cares")).andExpect(model().attributeHasErrors("stay"))
 				.andExpect(model().attributeHasFieldErrorCode("stay", "startdate", "dateStartDateIsPast"))
 				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
+	@Test
+	void testProcessNewStayFormHasErrorsExistAnotherWithSamePeriod() throws Exception {
+		mockMvc.perform(post("/owners/1/pets/{petId}/stays/new", TEST_PET_ID_2).with(csrf())
+				.param("startdate", "2020/11/10").param("finishdate", "2020/11/24").param("price", "50.0")
+				.param("specialCares", "A lot of special cares")).andExpect(model().attributeHasErrors("stay"))
+				.andExpect(model().attributeHasFieldErrorCode("stay", "finishdate", "duplicatedStay"))
+				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
+	}
+
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testShowStays() throws Exception {
 		mockMvc.perform(get("/owners/*/pets/{petId}/stays", TEST_PET_ID_1)).andExpect(status().isOk())
@@ -138,14 +97,13 @@ public class StayControllerTest {
 	}
 
 	// EDITAR ESTANCIA
-	
-	@WithMockUser(value = "spring")
+
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testInitEditStayForm() throws Exception {
 		mockMvc.perform(get("/owners/1/pets/{petId}/stays/{stayId}/edit", TEST_PET_ID_1, TEST_STAY_ID_1))
 				.andExpect(status().isOk()).andExpect(model().attributeExists("stay"))
 				.andExpect(model().attributeExists("pet"))
-				.andExpect(model().attribute("pet", this.clinicService.findPetById(TEST_PET_ID_1)))
 				.andExpect(model().attribute("stay", hasProperty("startdate", is(LocalDate.of(2020, 07, 15)))))
 				.andExpect(model().attribute("stay", hasProperty("finishdate", is(LocalDate.of(2020, 07, 19)))))
 				.andExpect(model().attribute("stay", hasProperty("specialCares", is("test special cares"))))
@@ -154,7 +112,7 @@ public class StayControllerTest {
 				.andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessEditStayFormSuccess() throws Exception {
 		mockMvc.perform(post("/owners/1/pets/{petId}/stays/{stayId}/edit", TEST_PET_ID_2, TEST_STAY_ID_1).with(csrf())
@@ -163,7 +121,7 @@ public class StayControllerTest {
 				.andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/owners/{ownerId}"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessEditStayFormHasErrors() throws Exception {
 		mockMvc.perform(post("/owners/1/pets/{petId}/stays/{stayId}/edit", TEST_PET_ID_2, TEST_STAY_ID_1).with(csrf())
@@ -174,51 +132,40 @@ public class StayControllerTest {
 				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
 
-	@WithMockUser(value = "spring")
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessEditStayFormHasErrorsExistAnotherWithSamePeriod() throws Exception {
-		mockMvc.perform(post("/owners/1/pets/{petId}/stays/{stayId}/edit", TEST_PET_ID_2, TEST_STAY_ID_2).with(csrf())
-				.param("startdate", "2020/07/18").param("finishdate", "2020/07/24").param("id", "2")
+		mockMvc.perform(post("/owners/1/pets/{petId}/stays/{stayId}/edit", TEST_PET_ID_2, TEST_STAY_ID_5).with(csrf())
+				.param("startdate", "2020/05/22").param("finishdate", "2020/05/27").param("id", "2")
 				.param("price", "50.0").param("specialCares", "A lot of special cares"))
 				.andExpect(model().attributeHasErrors("stay"))
 				.andExpect(model().attributeHasFieldErrorCode("stay", "finishdate", "duplicatedStay"))
 				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
-	
-	@WithMockUser(value = "spring")
+
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessEditStayFormHasErrorsDatesNull() throws Exception {
 		mockMvc.perform(post("/owners/1/pets/{petId}/stays/{stayId}/edit", TEST_PET_ID_2, TEST_STAY_ID_2).with(csrf())
-				.param("id", "2")
-				.param("price", "50.0").param("specialCares", "A lot of special cares"))
-				.andExpect(model().attributeHasErrors("stay"))
-				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
+				.param("id", "2").param("price", "50.0").param("specialCares", "A lot of special cares"))
+				.andExpect(model().attributeHasErrors("stay")).andExpect(status().isOk())
+				.andExpect(view().name("pets/createOrUpdateStayForm"));
 	}
-	
+
 	// DAR POR FINALIZADA ESTANCIA
-	
-	@WithMockUser(value = "spring")
+
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessEndStaySuccess() throws Exception {
-		mockMvc.perform(get("/owners/1/pets/1/stays/{stayId}/end", TEST_STAY_ID_4)).andExpect(status().is3xxRedirection())
-				.andExpect(view().name("redirect:/owners/{ownerId}"));
+		mockMvc.perform(get("/owners/6/pets/7/stays/{stayId}/end", TEST_STAY_ID_3))
+				.andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:/owners/{ownerId}"));
 	}
-	
-	@WithMockUser(value = "spring")
+
+	@WithMockUser(username = "admin1", authorities = "admin")
 	@Test
 	void testProcessEndStayError() throws Exception {
-		mockMvc.perform(get("/owners/1/pets/1/stays/{stayId}/end", TEST_STAY_ID_3)).andExpect(status().isOk())
+		mockMvc.perform(get("/owners/10/pets/13/stays/{stayId}/end", TEST_STAY_ID_7)).andExpect(status().isOk())
 				.andExpect(view().name("/exception"));
 	}
-	
-	@WithMockUser(value = "spring")
-	@Test
-	void testProcessNewStayFormHasErrorsExistAnotherWithSamePeriod() throws Exception {
-		mockMvc.perform(post("/owners/1/pets/{petId}/stays/new", TEST_PET_ID_2).with(csrf())
-				.param("startdate", "2020/07/21").param("finishdate", "2020/07/24")
-				.param("price", "50.0").param("specialCares", "A lot of special cares"))
-				.andExpect(model().attributeHasErrors("stay"))
-				.andExpect(model().attributeHasFieldErrorCode("stay", "finishdate", "duplicatedStay"))
-				.andExpect(status().isOk()).andExpect(view().name("pets/createOrUpdateStayForm"));
-	}
+
 }
